6b6bb56ee36ab1cdc8b2ec0f10e91243790e487b
AvidanSab <avidansabnani@gmail.com>
2026-04-21 08:50:51 -0400

    transformations

--- src/main.py
+++ src/main.py
@@ -2,9 +2,22 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Reads/visualized Data
 data = pd.read_csv('tides.csv')
 
 y_data = data['Predicted (ft)'].astype(float).values
 x_hours = np.arange(len(y_data))/10
 
-plt.plot(x_hours, )
+plt.plot(x_hours, y_data)
+plt.xlabel('Hours')
+plt.ylabel('Wave Height')
+
+
+
+max_height = np.max(y_data)
+min_height = np.min(y_data)
+
+amplitude = (max_height-min_height)/2
+v_shift = (max+min)/2
+
+plt.show()
