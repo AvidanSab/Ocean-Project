a85b1c8f2334975d51344bc71733e7f97089d3d7
AvidanSab <avidansabnani@gmail.com>
2026-04-21 09:02:28 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,22 +2,25 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Reads/visualized Data
+#Read/visulize the data
 data = pd.read_csv('tides.csv')
-
 y_data = data['Predicted (ft)'].astype(float).values
-x_hours = np.arange(len(y_data))/10
+x_hours = np.arange(len(y_data)) / 10
 
-plt.plot(x_hours, y_data)
-plt.xlabel('Hours')
-plt.ylabel('Wave Height')
-
-
+plt.plot(x_hours, y_data, color='black', label='Raw NOAA Data')
 
 max_height = np.max(y_data)
 min_height = np.min(y_data)
 
-amplitude = (max_height-min_height)/2
-v_shift = (max_height+min_height)/2
-plt.plot(v_shift, 'r--')
+amplitude = (max_height - min_height) / 2
+v_shift = (max_height + min_height) / 2
+
+print("Calculated Amplitude (a):", amplitude)
+print("Calculated Vertical Shift (k):", v_shift)
+
+#midline
+plt.axhline(y=v_shift, color='red', linestyle='--', label='Vertical Shift (k)')
+plt.legend()
+
+
 plt.show()
