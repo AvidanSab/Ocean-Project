c47736ff0a3e2bf30d70603f12fbe29173552965
AvidanSab <avidansabnani@gmail.com>
2026-04-22 11:54:52 -0400

    calculated period and made model.

--- src/main.py
+++ src/main.py
@@ -15,12 +15,25 @@ min_height = np.min(y_data)
 amplitude = (max_height - min_height) / 2
 v_shift = (max_height + min_height) / 2
 
+period = (2*3.14)/12.4
 
+#Gemini Code
+peak_index = np.argmax(y_data)
+h = x_hours[peak_index]
+
+y_model = amplitude * np.cos(period * (x_hours - h)) + v_shift
+
+print("Calculated Period factor (b):", period)
+print("Calculated Phase Shift (h):", h)
 print("Calculated Amplitude (a):", amplitude)
 print("Calculated Vertical Shift (k):", v_shift)
+
+plt.plot(x_hours, y_model, color='blue', linewidth=2, label='Math Model (Cosine)')
 
 #midline
 plt.axhline(y=v_shift, color='red', linestyle='--', label='Vertical Shift (k)')
 plt.legend()
+plt.xlabel('Hours')
+plt.ylabel('Height')
 
 plt.show()
